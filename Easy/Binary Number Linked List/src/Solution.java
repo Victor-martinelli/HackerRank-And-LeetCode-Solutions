571c9a38ad43e6efcca63b359bd43003e1325047
victor-Martinelli <vicbomlen@gmail.com>
2019-10-12 19:28:37 -0700

    Added Binary Number in Linked List

---
+++ Easy/Binary Number Linked List/src/Solution.java
@@ -0,0 +1,81 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * Given a singly linked list of 0s and 1s find its decimal equivalent.
+
+   Input  : 0->0->0->1->1->0->0->1->0
+   Output : 50   
+
+   Input  : 1->0->0
+   Output : 4
+Decimal Value of an empty linked list is considered as 0.
+ */
+
+/**
+ *
+ * @author Portatil
+ */
+public class Solution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Node n1 = new Node(0);
+        Node n2 = new Node(1);
+        Node n3 = new Node(1);
+        Node n4 = new Node(1);
+        Node n5 = new Node(0);
+        
+        n1.next=n2;
+        n2.next=n3;
+        n3.next=n4;
+        n4.next=n5;
+        
+        System.out.println(binaryToDecimal(n1));
+    }
+    
+    public static int binaryToDecimal(Node start)
+    {
+        
+        Node current = start;
+        
+        //We first store the binary number into a list
+        
+        List<Integer> numbers = new ArrayList();
+        
+        //While we have not reached the end
+        
+        while(current!=null)
+        {
+            numbers.add(current.data);
+            
+            current=current.next;
+            
+        }
+        
+        //We now go through the list calculating the binary number
+        
+        int result=0;
+        
+        for(int i=0;i<numbers.size();i++)
+        {
+            if(numbers.get(i)==1)
+                result+=Math.pow(2,numbers.size()-1-i);
+        }
+        
+        return result;
+    }
+    
+    public static void printList(Node current)
+    {
+        while(current!=null)
+            System.out.print(current.data+" ");
+        
+        System.out.println();
+    }
+    
+}
---
+++ Medium/Test/src/test/Test.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Portatil
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    
+    public static boolean compare(word1, word2)
+        // this should return True if value 1 is alphabetically before value 2, false otherwise
+        letter_index = 0
+        while letter_index < word1.length()
+            letter_index += 1
+            letter1 = word1.get_character_at(letter_index)
+            letter2 = word2.get_character_at(letter_index)
+            if alphabet.indexOf(letter1) > alphabet.indexOf(letter2)
+                return True
+            else if alphabet.indexOf(letter2) > alphabet.indexOf(letter1)
+                return False
+        // if they’re the same word, we’ll exit the loop and it doesn’t matter what we return
+        return True
+    
+}
